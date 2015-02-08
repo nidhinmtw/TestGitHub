9d9f52de6f2e392e9fdc22ade91e5a2084517238
nidhinmtw <nidhinmtw@gmail.com>
2015-02-08 18:18:24 +0530

    Nodejs beginners lessons commited. With Lesson 4, POST request processing gets almost over. Not it has to be handled at the amateaur level in later lessons

--- nodejs/Lesson 13/requesthandlers.js
+++ nodejs/Lesson 13/requesthandlers.js
@@ -1,12 +1,8 @@
-// Lesson 14
+// Lesson 13
 // --------
 // Lets try to give some content out to be drawn to the page when the start function is called and this function should serve data to the upload function.
 // As for this lesson we will create a textarea and take its bigggggg content entered by the user to process further.
 // THis will mainly show us data coming in chunks until the last end chunk comes in.
-
-// Using querystring module to handle the field in the data
-
-var querystring = require("querystring");
 
 function start(response, postData)
 {
@@ -18,7 +14,6 @@ function start(response, postData)
 					"<body>"+
 						"<form action='/upload' method='post'>"+
 							"<textarea name=text rows=20 cols=60></textarea>"+
-							"<input type='text' name='urname' value='Your name goes here'/>"+
 							"<input type='submit' value='Submit Text'/>"+
 						"</form>"+
 					"</body>"+
@@ -30,9 +25,8 @@ function start(response, postData)
 function upload(response, postData)
 {
 	console.log("Request handler upload was called.");
-	response.writeHead(200, {'Content-Type':'text/html'})
-	response.write('Hello upload... Lesson 14!!! You have sent field/text ::  '+ querystring.parse( postData).text );
-	response.write('</br>Hello upload... Lesson 14!!! You have sent field/urname ::  '+ querystring.parse( postData).urname );
+	response.writeHead(200, {'Content-Type':'text/plain'})
+	response.write('Hello upload... Lesson 13!!! You have sent '+ postData );
 	response.end();
 }
 
