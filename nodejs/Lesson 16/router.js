b2cd91aa42701452df8268edbc7fed5a543320d4
nidhinmtw <nidhinmtw@gmail.com>
2015-02-08 20:59:16 +0530

    Lesson 16:- We did a image file show on the browser with this

---
+++ nodejs/Lesson 16/router.js
@@ -0,0 +1,33 @@
+// Lesson 16
+// --------
+// Create the router to handle the individual requests.
+// It is not the job of the web server module to handle each and every requests independtly.
+// Start taking the handle map for requesthandling
+function route(handle, pathname, response, postData)
+{
+	// Avoiding unnecessary consoles for the default requests for favicon.ico. This is not part of the lesson or intentional.
+	// Just for convenience of the terminal message.
+	if(pathname !== '/favicon.ico')
+	{
+		console.log("About to handle a route a request for "+ pathname);
+	}
+	if(typeof handle[pathname] === 'function')
+	{
+		handle[pathname](response, postData);//invoke the handler function as per the path name parsed
+	}
+	else
+	{
+		// Avoiding unnecessary consoles for the default requests for favicon.ico. This is not part of the lesson or intentional.
+		// Just for convenience of the terminal message.
+		if(pathname !== '/favicon.ico')
+		{
+			console.log("No request handler found for "+pathname);
+		
+		}
+		response.writeHead(404, {'Content-Type':'text/plain'})
+		response.write('404 Not found');
+		response.end();
+
+	}
+}
+exports.route = route;
---
+++ nodejs/Lesson 16/server.js
@@ -0,0 +1,33 @@
+// Lesson 16
+// --------
+// Now when user sends us some data, depending on the size of the data, its gonna come in multi-chunks and one end chunk.
+// So we need to listen to each chunk and buffer it together until the end packet comes and then the full data can be passed on to the relevant execution handle for proper processing.
+var http = require('http');
+var url = require('url');
+
+// start handling the handle here
+function start(route, handle)
+{
+	function onRequest(request, response)
+	{
+		var pathName = url.parse(request.url).pathname;
+		if(pathName !== '/favicon.ico')
+		{
+			console.log('Got a request for '+pathName);
+		}
+		var postData = "";
+		request.setEncoding("utf8");
+		request.addListener("data", function(postDataChunk){
+			postData += postDataChunk;
+			console.log("Recieved a chunk ...!!!!!!!!!!!!!");
+		});
+		request.addListener("end", function(){
+			route(handle, pathName, response, postData);
+		});
+
+	}
+	var server = http.createServer(onRequest);
+	server.listen(8000);
+	console.log('Server started @ 8000. Try http://localhost:8000 to see the result.');
+}
+exports.start = start;
